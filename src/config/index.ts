b5112febec457d3a19c3fc9c97ab6ab931f64afe
KIBONGO Simon Peter <kibongos40@gmail.com>
2024-11-05 14:43:19 +0200

    Update the live URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  BASE_URL: import.meta.env.VITE_BASE_URL || "http://localhost:5000",
+  API_URL: import.meta.env.VITE_API_URL || "https://api.djpius.com/api",
+  BASE_URL: import.meta.env.VITE_BASE_URL || "https://api.djpius.com",
 };
